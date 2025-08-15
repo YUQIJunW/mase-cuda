Fixed MXFP4 precision issue

src/mase_cuda/mxfp6_E2M3/dequantize.py
@@ -71,7 +71,7 @@ def dequantize1d_E2M3_simulated(input: torch.Tensor, scale: torch.Tensor, group_
     frac = (fp6 & 0x07).to(torch.int16) << 4  # get the mantissa bits
 
     scales = scales.to(torch.int16)  # get the scale bits
-    result = exp + scales
+    result = scales - exp 
     result = torch.where(result < 0, 0, result)  # avoid negative exponent
     result = torch.where(result > 0xFE, 0xFE, result)  # avoid overflow
     exp = result << 7

src/mase_cuda/mxfp6_E2M3/quantize.py
@@ -3,7 +3,6 @@ import torch
 
 def quantize1d_E2M3_simulated(weights: torch.Tensor, group_size: int) -> tuple[torch.Tensor, torch.Tensor]:
     assert weights.ndim == 1, "Weights tensor must be 1D"
-    bias = 0x3
     assert group_size > 0, "Group size must be positive"
     numel = weights.numel()
     assert numel % group_size == 0, "Number of elements in the weights tensor must be divisible by the group size"
@@ -18,13 +17,13 @@ def quantize1d_E2M3_simulated(weights: torch.Tensor, group_size: int) -> tuple[t
 
     # Get the exponent bits
     exponent = (w_g.view(torch.int32) >> 23) & 0xFF
-    group_exp = exponent.max(dim=1, keepdim=True).values - bias
+    group_exp = exponent.max(dim=1, keepdim=True).values
     scales = group_exp.to(torch.uint8).flatten()
     
     w_g = w_g.to(torch.bfloat16).view(torch.int16)
     w_g_exp = (w_g & 0x7F80) >> 7
-    w_g_exp = w_g_exp - group_exp
-    w_g_exp = torch.where(w_g_exp < 0, 0, w_g_exp)
+    w_g_exp =  group_exp - w_g_exp
+    w_g_exp = torch.where(w_g_exp > 3, 3, w_g_exp)
     w_g_exp = (w_g_exp << 3 ) & 0x18
     w_g_flag = (w_g & 0x8) >> 3
     w_g_frac = (((w_g & 0x70) >> 4 ) + w_g_flag)

src/mase_cuda/mxfp8_E4M3/dequantize.py
@@ -48,8 +48,6 @@ def dequantize1d_E4M3_simulated(input: torch.Tensor, scale: torch.Tensor, group_
     assert scale.ndim == 1, "Scale tensor must be 1D"
     input = input.view(torch.uint8)
     scale = scale.view(torch.uint8)
-    bias = 0x7
-    final_bias = -bias
     numel = input.numel()
     num_groups = numel // group_size
 
@@ -60,7 +58,7 @@ def dequantize1d_E4M3_simulated(input: torch.Tensor, scale: torch.Tensor, group_
     frac = (fp8 & 0x07).to(torch.int16) << 4  # get the mantissa bits
 
     scales = scales.to(torch.int16)  # get the scale bits
-    result = exp + scales
+    result = scales - exp
     result = torch.where(result < 0, 0, result)  # avoid negative exponent
     result = torch.where(result > 0xFE, 0xFE, result)  # avoid overflow
     exp = result << 7  

src/mase_cuda/mxfp8_E5M2/dequantize.py
@@ -58,7 +58,7 @@ def dequantize1d_E5M2_simulated(input: torch.Tensor, scale: torch.Tensor, group_
     frac = (fp8 & 0x03).to(torch.int16) << 5  # get the mantissa bits
 
     scales = scales.to(torch.int16)  # get the scale bits
-    result = exp + scales
+    result = scales - exp
     result = torch.where(result < 0, 0, result)  # avoid negative exponent
     result = torch.where(result > 0xFE, 0xFE, result)  # avoid overflow
     exp = result << 7  

test/py/mxfp6_E3M2/linear/test_qlinear_mxfp6_E3M2.py
@@ -16,10 +16,10 @@ seed_everything(42)
 def test_packed_weight():
     num_random_tests = 10
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # shapes = [(1024,), (32, 32), (1024, 1024)]
-    # group_sizes = [4, 16, 64, 256, 1024]
-    shapes = [(1024,)]
-    group_sizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+    shapes = [(1024,), (32, 32), (1024, 1024)]
+    group_sizes = [4, 16, 64, 256, 1024]
+    # shapes = [(1024,)]
+    # group_sizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 
     rows = []
     rows_sim = []